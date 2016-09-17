a188532bb690da2c9fc4543db146d7086079973b
Luis Paulo Nery <l.pns@hotmail.com>
2016-09-16 22:07:48 -0300

    programa funcional

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 
 main() 
 {
@@ -19,6 +20,20 @@ main()
 	}
 	
 	printf ("O conteudo de memoria alocada e: %s", memoria_alocada);
+	
+	memoria_alocada = realloc (memoria_alocada, 100 * sizeof (char));
+	
+	if (memoria_alocada == NULL)
+	{
+		printf ("Nao foi possivel realocar a memoria desejada");
+	}
+	
+	else
+	{
+		strcpy (memoria_alocada, "Quantidade de memoria alocada alterada!\n");
+	}
+	
+	printf ("O conteudo de memoria realocada e : %s\n", memoria_alocada);
 	
 	free(memoria_alocada);
 	
